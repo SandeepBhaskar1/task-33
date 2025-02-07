1704b07d283379a62dc9bcee0eb52a4b130a0d90
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-02-07 17:38:22 +0530

    ab

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_CLOUD_URL,
+    origin: 'https://task-33-frontend.vercel.app',
     credentials: true
 }));
 
